5825a0bd20472fa6ece56fa06eecf6becfca1b83
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-06-05 01:11:57 +0200

    fixed gruntfile for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,7 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
+        'lib/*.js',
         '<%= nodeunit.tests %>'
       ],
       options: {
@@ -29,21 +30,23 @@ module.exports = function(grunt) {
     },
 
     // Configuration to be run (and then tested).
-    django_static: {
-      default_options: {
+    djangoStatic: {
+      css: {
         options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+          html: 'test/fixtures/css.html',
+          dest: 'tmp/css.html'
         }
       },
-      custom_options: {
+      cssCdn: {
         options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+          html: 'test/fixtures/cssCdn.html',
+          dest: 'tmp/cssCdn.html'
+        }
+      },
+      script: {
+        options: {
+          html: 'test/fixtures/script.html',
+          dest: 'tmp/script.html'
         }
       }
     },
@@ -63,11 +66,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'django_static', 'nodeunit']);
-
-  // By default, lint and run all tests.
+  // Test task
+  grunt.registerTask('test', ['clean', 'djangoStatic', 'nodeunit']);
   grunt.registerTask('default', ['jshint', 'test']);
-
 };
