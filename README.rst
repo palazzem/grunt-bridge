9b1b17890ef06c209140eff8c74137a3740ee341
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-06-04 20:09:19 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,9 @@
+Grunt django static
+===================
+
+Something big is really happening with this grunt task.
+
+License
+=======
+
+BSD-2
---
+++ tasks/django_static.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-django-static
+ * https://github.com/palazzem/grunt-django-static
+ *
+ * Copyright (c) 2014 Emanuele Palazzetti
+ * Licensed under the BSD license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('django_static', 'Convert all your js/css references to valid django static urls', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
