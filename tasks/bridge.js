be18563dcf44239307cb1b01ae972cc1698f65bb
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-06-08 00:11:40 +0200

    better separation for a better unittest

--- tasks/bridge.js
+++ tasks/bridge.js
@@ -9,10 +9,7 @@
 'use strict';
 
 module.exports = function (grunt) {
-  var xpath = require('xpath'),
-    DOMParser = require('xmldom').DOMParser,
-    defaultMime = 'text/html',
-    format = require('../lib/formatter').format;
+  var bridgeReplacement = require('../lib/replace');
 
   grunt.registerMultiTask('bridge', 'Convert all your js/css references to valid backend framework static urls', function () {
     // Collect files and options
@@ -22,30 +19,13 @@ module.exports = function (grunt) {
       dest: '.tmp/base.html'
     });
 
-    var nodes = [];
-
-    // Load and parse base file of your framework
+    // Load base file of your framework
     var baseHtml = grunt.file.read(options.html);
-    var dom = new DOMParser().parseFromString(baseHtml, defaultMime);
 
-    // Find <link> and <scripts>
-    nodes = nodes.concat(xpath.select("//link[not(contains(@href, '//'))]", dom));
-    nodes = nodes.concat(xpath.select("//script[not(contains(@src, '//'))]", dom));
-    grunt.log.writeln("Statics found: " + nodes.length);
-
-    nodes.forEach(function (value) {
-      // Grab correct selector
-      var originNode = xpath.select1('@href', value) || xpath.select1('@src', value);
-
-      // Change origin node with a new static url
-      var newPattern = format(options.pattern, {path: originNode.value});
-      var staticLink = originNode.toString().replace(originNode.value, newPattern);
-
-      baseHtml = baseHtml.replace(originNode, staticLink);
-      grunt.log.write(originNode.value + "...").ok();
-    });
+    // Generate new html
+    var newHtml = bridgeReplacement(baseHtml, options, grunt.log.write);
 
     // Write base html to a new file according to destination
-    grunt.file.write(options.dest, baseHtml);
+    grunt.file.write(options.dest, newHtml);
   });
 };
