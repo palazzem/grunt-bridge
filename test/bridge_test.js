285b39725957e2e75ea023843b3c1501d3e3dfe4
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2014-06-08 00:47:55 +0200

    nodeunite doesn't use grunt task to test replacement

--- test/bridge_test.js
+++ test/bridge_test.js
@@ -1,15 +1,21 @@
 'use strict';
 
 var grunt = require('grunt');
+var requireHelper = require('../lib/helpers');
 
-exports.djangoStatic = {
-  setUp: function(done) {
-    done();
-  },
+var bridgeReplacement = requireHelper('lib/replace');
+
+exports.bridge = {
   css: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/css.html');
+    var options = {
+      html: 'test/fixtures/css.html',
+      pattern: '{% static \'{path}\' %}',
+      dest: 'tmp/css.html'
+    };
+
+    var actual = bridgeReplacement(grunt.file.read('test/fixtures/css.html'), options);
     var expected = grunt.file.read('test/expected/css.html');
     test.equal(actual, expected, 'should change a link to a valid static link');
 
@@ -18,7 +24,13 @@ exports.djangoStatic = {
   cssCdn: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/cssCdn.html');
+    var options = {
+      html: 'test/fixtures/cssCdn.html',
+      pattern: '{% static \'{path}\' %}',
+      dest: 'tmp/cssCdn.html'
+    };
+
+    var actual = bridgeReplacement(grunt.file.read('test/fixtures/cssCdn.html'), options);
     var expected = grunt.file.read('test/expected/cssCdn.html');
     test.equal(actual, expected, 'should change only local links but not CDNs');
 
@@ -27,7 +39,13 @@ exports.djangoStatic = {
   script: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/script.html');
+    var options = {
+      html: 'test/fixtures/script.html',
+      pattern: '{% static \'{path}\' %}',
+      dest: 'tmp/script.html'
+    };
+
+    var actual = bridgeReplacement(grunt.file.read('test/fixtures/script.html'), options);
     var expected = grunt.file.read('test/expected/script.html');
     test.equal(actual, expected, 'should change only local scripts but not CDNs');
 
@@ -36,7 +54,13 @@ exports.djangoStatic = {
   differentPattern: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/pattern.html');
+    var options = {
+      html: 'test/fixtures/pattern.html',
+      pattern: '{{# url \'{path}\' }}',
+      dest: 'tmp/pattern.html'
+    };
+
+    var actual = bridgeReplacement(grunt.file.read('test/fixtures/pattern.html'), options);
     var expected = grunt.file.read('test/expected/pattern.html');
     test.equal(actual, expected, 'should use a different pattern');
 
